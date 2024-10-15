d8512411c330cb452c7be5080cdea60da32fe2f1
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2024-10-15 04:51:43 +0200

    Update unit tests.

---
+++ test/EgonsoftHU.Text.Json.UnitTests/Stubs/JsonStringEnumMemberNameAttribute.cs
@@ -0,0 +1,31 @@
+﻿// Copyright © 2023-2024 Gabor Csizmadia
+// This code is licensed under MIT license (see LICENSE for details)
+
+#if EGONSOFTHU_TEXT_JSON_INCLUDE_JSONSTRINGENUMMEMBERNAMEATTRIBUTE
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Text.Json.Serialization
+{
+    /// <summary>
+    /// Determines the string value that should be used when serializing an enum member.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class JsonStringEnumMemberNameAttribute : Attribute
+    {
+        /// <summary>
+        /// Creates new attribute instance with a specified enum member name.
+        /// </summary>
+        /// <param name="name">The name to apply to the current enum member.</param>
+        public JsonStringEnumMemberNameAttribute(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// Gets the name of the enum member.
+        /// </summary>
+        public string Name { get; }
+    }
+}
+#endif
